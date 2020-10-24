a82b639827ecb9f49a81a4ec240da203cdbc5cf3
Jaivrat12 <56949902+Jaivrat12@users.noreply.github.com>
2020-10-24 11:19:22 +0530

    Update auth.php

--- auth.php
+++ auth.php
@@ -2,7 +2,6 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 $_POST = json_decode(file_get_contents('php://input'), true);
 
